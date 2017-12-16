7d9434e16ecbecb63519da10ab9bee960227372e
iMac_Home <helloted@live.com>
2017-12-16 17:31:37 +0800

    去除广告

--- HTBlocker/HTBlocker/ViewController.m
+++ HTBlocker/HTBlocker/ViewController.m
@@ -8,10 +8,19 @@
 
 #import "ViewController.h"
 #import <Masonry/Masonry.h>
-#import "HTTableViewCell.h"
 
+#define RGBFromHex(rgbValue) \
+[UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
+blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
+alpha:1.0]
 
-@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+#define UI_WIDTH    [UIScreen mainScreen].bounds.size.width
+#define Margin      20
+
+@interface ViewController ()
+
+@property (nonatomic, strong)UIScrollView    *scrollView;
 
 @end
 
@@ -19,62 +28,129 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _scrollView = [[UIScrollView alloc]initWithFrame:self.view.bounds];
+    [self.view addSubview:_scrollView];
+    _scrollView.showsVerticalScrollIndicator = NO;
     
-    UITableView *tableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStylePlain];
-    tableView.delegate = self;
-    tableView.dataSource = self;
-    tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    [self.view addSubview:tableView];
+    
+    UILabel *label_1 = [[UILabel alloc]init];
+    label_1.font = [UIFont boldSystemFontOfSize:16];
+    label_1.textColor = RGBFromHex(0x444444);
+    [_scrollView addSubview:label_1];
+    [label_1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_scrollView.mas_left).offset(Margin);
+        make.right.equalTo(_scrollView.mas_right).offset(-Margin);
+        make.top.equalTo(_scrollView.mas_top).offset(30);
+        make.height.equalTo(@30);
+    }];
+    label_1.text = @"第一步，进入设置";
+    
+    UIImage *image_1 = [UIImage imageNamed:@"01"];
+    UIImageView *imageView_1 = [[UIImageView alloc]initWithImage:image_1];
+    imageView_1.contentMode = UIViewContentModeScaleAspectFit;
+    [self.scrollView addSubview:imageView_1];
+    [imageView_1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(label_1.mas_left);
+        make.top.equalTo(label_1.mas_bottom).offset(5);
+        make.right.equalTo(self.view.mas_right).offset(-Margin);
+        make.height.equalTo(@([self convertWithImage:image_1]));
+    }];
     
 
-//
-//
-//    UILabel *label_2 = [[UILabel alloc]init];
-//    label_2.textColor = RGBFromHex(0x666666);
-//    [self.scrollview addSubview:label_2];
-//    [label_2 mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(self.scrollview.mas_left).offset(20);
-//        make.right.equalTo(self.scrollview.mas_right).offset(-20);
-//        make.top.equalTo(image_1.mas_bottom).offset(20);
-//        make.height.equalTo(@30);
-//    }];
-//    label_2.text = @"第二步，选择Safari APP";
-//
-//    UIImageView *image_2 = [[UIImageView alloc]init];
-//    [self.scrollview addSubview:image_2];
-//    [image_2 mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(label_1.mas_left);
-//        make.top.equalTo(label_2.mas_bottom).offset(10);
-//        make.right.equalTo(label_1.mas_right);
-//        make.height.equalTo(@100);
-//    }];
-//    image_2.backgroundColor = [UIColor lightGrayColor];
+    UILabel *label_2 = [[UILabel alloc]init];
+    label_2.font = [UIFont boldSystemFontOfSize:16];
+    label_2.textColor = RGBFromHex(0x444444);
+    [_scrollView addSubview:label_2];
+    [label_2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_scrollView.mas_left).offset(Margin);
+        make.right.equalTo(_scrollView.mas_right).offset(-Margin);
+        make.top.equalTo(imageView_1.mas_bottom).offset(15);
+        make.height.equalTo(@30);
+    }];
+    label_2.text = @"第二步，选择Safari设置";
+    
+    UIImage *image_2 = [UIImage imageNamed:@"02"];
+    UIImageView *imageView_2 = [[UIImageView alloc]initWithImage:image_2];
+    imageView_2.contentMode = UIViewContentModeScaleAspectFit;
+    [self.scrollView addSubview:imageView_2];
+    [imageView_2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(label_1.mas_left);
+        make.top.equalTo(label_2.mas_bottom).offset(5);
+        make.right.equalTo(self.view.mas_right).offset(-Margin);
+        make.height.equalTo(@([self convertWithImage:image_2]));
+    }];
+    
+    
+    UILabel *label_3 = [[UILabel alloc]init];
+    label_3.textColor = RGBFromHex(0x444444);
+    label_3.font = [UIFont boldSystemFontOfSize:16];
+    [_scrollView addSubview:label_3];
+    [label_3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_scrollView.mas_left).offset(Margin);
+        make.right.equalTo(_scrollView.mas_right).offset(-Margin);
+        make.top.equalTo(imageView_2.mas_bottom).offset(15);
+        make.height.equalTo(@30);
+    }];
+    label_3.text = @"第三步，选择内容拦截器";
+    
+    UIImage *image_3 = [UIImage imageNamed:@"03"];
+    UIImageView *imageView_3 = [[UIImageView alloc]initWithImage:image_3];
+    imageView_3.contentMode = UIViewContentModeScaleAspectFit;
+    [self.scrollView addSubview:imageView_3];
+    [imageView_3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(label_1.mas_left);
+        make.top.equalTo(label_3.mas_bottom).offset(5);
+        make.right.equalTo(self.view.mas_right).offset(-Margin);
+        make.height.equalTo(@([self convertWithImage:image_3]));
+    }];
+    
+    UILabel *label_4 = [[UILabel alloc]init];
+    label_4.textColor = RGBFromHex(0x444444);
+    label_4.font = [UIFont boldSystemFontOfSize:16];
+    [_scrollView addSubview:label_4];
+    [label_4 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_scrollView.mas_left).offset(Margin);
+        make.right.equalTo(_scrollView.mas_right).offset(-Margin);
+        make.top.equalTo(imageView_3.mas_bottom).offset(15);
+        make.height.equalTo(@30);
+    }];
+    label_4.text = @"第四步，打开广告拦截的开关";
+    
+    UIImage *image_4 = [UIImage imageNamed:@"04"];
+    UIImageView *imageView_4 = [[UIImageView alloc]initWithImage:image_4];
+    imageView_4.contentMode = UIViewContentModeScaleAspectFit;
+    [self.scrollView addSubview:imageView_4];
+    [imageView_4 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(label_1.mas_left);
+        make.top.equalTo(label_4.mas_bottom).offset(5);
+        make.right.equalTo(self.view.mas_right).offset(-Margin);
+        make.height.equalTo(@([self convertWithImage:image_4]));
+    }];
+    
+    
+    UILabel *label_5 = [[UILabel alloc]init];
+    label_5.textColor = RGBFromHex(0x444444);
+    label_5.font = [UIFont boldSystemFontOfSize:16];
+    [_scrollView addSubview:label_5];
+    [label_5 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_scrollView.mas_left).offset(Margin);
+        make.right.equalTo(_scrollView.mas_right).offset(-Margin);
+        make.top.equalTo(imageView_4.mas_bottom).offset(15);
+        make.height.equalTo(@30);
+    }];
+    label_5.text = @"至此，已经成功拦截浏览器广告";
 
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 4;
-}
-
-- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath
-{
-    return 120;
+- (CGFloat)convertWithImage:(UIImage *)image{
+    CGFloat width = image.size.width;
+    CGFloat height = image.size.height;
+    return height/width * (UI_WIDTH - 2 * Margin);
 }
 
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    HTTableViewCell *cell = [HTTableViewCell cellWithTableView:tableView];
-    if (indexPath.row == 0) {
-        cell.title = @"第一步，进入设置APP";
-    }else if (indexPath.row == 1){
-        cell.title = @"第二步，选择Safari APP";
-    }else if (indexPath.row == 2){
-        cell.title = @"第三步，选择Safari APP";
-    }else if (indexPath.row == 3){
-        cell.title = @"第四步，选择Safari APP";
-    }
-    return cell;
+- (void)viewDidAppear:(BOOL)animated{
+   _scrollView.contentSize = CGSizeMake(_scrollView.bounds.size.width, _scrollView.bounds.size.height+1);
 }
 
 
